d233a11d29b59494a897e86bdb8de896b7be3dfb
fewalthel <fewalthel@gmail.com>
2025-05-01 12:50:40 +0300

    doing sliders

--- src/pages/MainPage/Sliders/index.tsx
+++ src/pages/MainPage/Sliders/index.tsx
@@ -1,21 +1,69 @@
 import styles from './index.module.scss'
 import {Link} from "react-router-dom";
+import {useRef} from "react";
 
 export const Sliders = () => {
-    return (<>
-            <button>влево</button>
-            <button>вправо</button>
-            <div className={styles.sliders_container}>
+
+
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const handleRef = (vw: number) => {
+        if (containerRef.current) {
+            const px = (vw * window.innerWidth) / 100;
+            containerRef.current.scrollBy({left: px, behavior: 'smooth'});
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.container_for_scroll_buttons}>
+                <button onClick={() => handleRef(-79.628)}>
+                    <img src='/icons/arrow-left.svg'/>
+                </button>
+                <button onClick={() => handleRef(79.628)}>
+                    <img src='/icons/arrow-right.svg'/>
+                </button>
+            </div>
+            <div className={styles.sliders_container} ref={containerRef}>
+
+                <div className={styles.first_slider}>
+                    <div className={styles.slider_info}>
+                        <h1>Сделаем мир чище</h1>
+                        <h2>Сдай макулатуру или старую одежду и получи скидку на покупку товаров из переработанных
+                            материалов</h2>
+                        <Link to='/' className={styles.button}>Условия сервиса</Link>
+                    </div>
+                    <div className={styles.image}>
+                        <img src='/recycling-concept-flat-lay%202%202.png'/>
+                    </div>
+                </div>
+
                 <div className={styles.yellow_slider}>
-                    <div style={{marginTop:"2.75vw", marginLeft:'4vw', height:'13vw'}}>
+                    <div className={styles.slider_info}>
                         <h1>А вы знали...</h1>
                         <h2>что среднее время разложения пластмассовых изделий колеблется от 400 до 700 лет, а
                             полиэтиленовых пакетов — от 100 до 200 лет? </h2>
                         <Link to='/' className={styles.button}>Узнать больше</Link>
                     </div>
-                    <img src='/слайд2%201.png'/>
+                    <div className={styles.image}>
+                        <img src='/слайд2%201.png'/>
+                    </div>
+                </div>
+
+                <div className={styles.green_slider}>
+                    <div className={styles.slider_info_second}>
+                        <h1>Что с масками?</h1>
+                        <h2>Медицинские маски не обязательно должны становиться отходами. Их тоже можно сдать на
+                            переработку.</h2>
+
+                        <Link to='/' className={styles.button}>Пункты сбора масок</Link>
+                    </div>
+                    <div className={styles.image}>
+                        <img src='/слайд3%201.png'/>
+                    </div>
                 </div>
             </div>
-        </>
+
+        </div>
     )
 }
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,10 +1,12 @@
 import {Sliders} from "./Sliders";
 import './index.css'
 
+
 export const MainPage = () => {
     return <main>
         main
 
         <Sliders/>
+
     </main>
 }
